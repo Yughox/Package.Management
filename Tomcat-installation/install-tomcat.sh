bfb95b8cf0c8be045cbf3e1b44c37a41a20d1b1e
Ugo Uche <120284311+Yughox@users.noreply.github.com>
2023-07-05 17:58:34 -0700

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -11,9 +11,9 @@ sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
-sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.73/bin/apache-tomcat-9.0.73.zip 
-sudo tar -xvf apache-tomcat-9.0.73.tar.gz
-sudo rm -rf apache-tomcat-9.0.73.tar.gz
+sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.tar.gz
+sudo tar -xvf apache-tomcat-9.0.76.tar.gz
+sudo rm -rf apache-tomcat-9.0.76.tar.gz
 ### rename tomcat for good naming convention
 sudo mv apache-tomcat-9.0.73 tomcat9
 ### assign executable permissions to the tomcat home directory
